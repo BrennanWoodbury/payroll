558403b8a933169aad488f85e1b02ec4a68e6878
Brennan Woodbury <bthedub@gmail.com>
2020-01-30 23:51:53 -0700

    added classes

---
+++ employee.py
@@ -0,0 +1,3 @@
+class Employee(first_name, last_name, is_comission, is_hourly, pay_rate):
+
+
--- payroll.py
+++ payroll.py
@@ -1,19 +1,47 @@
 
 #function to calculate total weekly earnings.
-def hourly():
+def hourly_1():
     
     pay_rate = input("Employee's Hourly Wage: ")
     hours = input("Hours worked: ")
+    reg_pay = float(hours) * float(pay_rate)
+    otime_pay = 40 * float(pay_rate) + (float(hours) - 40) * (float(pay_rate) * 1.5)
+
 
     if float(hours) <= 40:
-        print( float(hours) * float(pay_rate))
+        print(reg_pay)
     else:
-        print( 40 * float(pay_rate) + (float(hours) - 40) * (float(pay_rate) * 1.5))
-        return 
+        print(otime_pay)
+    return
+     
 
-hourly()
+hourly_1()
 
-ok
-Pcom
+#function to calculate total weekly earnings.
+def hourly_2():
+    
+    pay_rate = input("Employee's Hourly Wage: ")
+    hours = input("Hours worked: ")
+    reg_pay = float(hours) * float(pay_rate)
+    otime_pay = 40 * float(pay_rate) + (float(hours) - 40) * (float(pay_rate) * 1.5)
 
 
+    if float(hours) <= 40:
+        print(reg_pay)
+    else:
+        print(otime_pay)
+    return
+        
+
+hourly_2()
+
+#function to calculate paycheck over 2 weeks. 
+def pay_check(hourly_1, hourly_2):
+    hourly_1 = {}
+    hourly_2 = {}
+
+    print(hourly_1 + hourly_2)
+    return
+    
+
+pay_check()
